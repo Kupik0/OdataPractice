4f41b94c748cfa08b73375c35f1db286ce056824
Mustafa Kupik <111908528+Kupik0@users.noreply.github.com>
2023-04-01 17:05:38 +0300

    Add project files.

---
+++ ODATApractice.WebApi/Infrastructure/Context/StudentDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ODATApractice.WebApi.Infrastructure.DbEntities;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace ODATApractice.WebApi.Infrastructure.Context;
+
+public class StudentDbContext : DbContext
+{
+    public StudentDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Grade>()
+            .HasMany<Student>(g => g.Students)
+            .WithOne(s => s.Grade)
+            .HasForeignKey(s => s.CurrentGradeId);
+    }
+}
---
+++ ODATApractice.WebApi/Infrastructure/DbEntities/Grade.cs
@@ -0,0 +1,10 @@
+﻿namespace ODATApractice.WebApi.Infrastructure.DbEntities;
+
+public class Grade
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public virtual ICollection<Student> Students { get; set; }
+}
